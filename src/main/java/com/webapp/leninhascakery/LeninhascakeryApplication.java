b0b1c28e0265bc51775c81799b441a2a598fd223
Dayane Roson <dayane.roson@gmail.com>
2022-01-15 15:00:52 -0300

    initial commit

---
+++ src/main/java/com/webapp/leninhascakery/LeninhascakeryApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.leninhascakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeninhascakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeninhascakeryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/leninhascakery/LeninhascakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.leninhascakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeninhascakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
